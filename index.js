e4d5b7bbb55aa7c2e4de5c8e7281ae7f9957f543
adham7700 <madham7700@gmail.com>
2026-08-16 22:31:32 +0300

    .

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ app.listen(5000,()=>{
   console.log('Server is running on port 5000');
 })
 const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+} );
